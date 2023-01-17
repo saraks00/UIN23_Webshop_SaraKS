c7e47abb4a9da3129191495b2991100577d7fc1b
saraks00 <sarakristine@hotmail.no>
2023-01-17 17:30:06 +0100

    endringer på prosjekt

---
+++ shopscript.js
@@ -0,0 +1,28 @@
+let cart = []
+
+function addToCart(title, price) {
+    cart.push({productTitle: title, productPrice: price, productQuantity: 1})
+
+    console.log(cart)
+    renderCart()
+    //oppdatere label med antall produkter
+    document.querySelector("#cart .label").innerHTML = cart.length
+}
+
+function renderCart() {
+    //tom variabel for å bygge HTML til produkter
+    let listHTML = ""
+    //gp gjennom cart-arrayen, lag <li> for hvert produkt
+    cart.map(prod => listHTML+= `<li>
+    <span class="title">${prod.productTitle}</span>
+    <span class="price">${prod.productPrice}</span>
+    <span class="quantity">${prod.productQuantity}</span>
+    <button class="delete">X</button>
+    </li>`)
+    //bruke en selektor for å finne riktig <ul>, og skrive inn listHTML:
+    document.querySelector("#cartview ul").innerHTML = listHTML
+}
+
+function toggleCart() {
+    document.querySelector("#cartview").classList.toggle("hidden")
+}
