bf19ab5e334ca84dbdb82b9824806a1c4228fb68
saraks00 <sarakristine@hotmail.no>
2023-01-20 08:55:07 +0100

    endfringer

--- shopscript.js
+++ shopscript.js
@@ -1,6 +1,6 @@
 let cart = []
 
-function addToCart(title, price) {
+function add(title, price) {
     cart.push({productTitle: title, productPrice: price, productQuantity: 1})
 
     console.log(cart)
